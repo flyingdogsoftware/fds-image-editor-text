0a23264d0e9c7c2674de298b6880d8dacda379c4
Marcin Grzegorzewski <grzegorzewski@flyingdog.de>
2024-07-12 22:39:26 +0200

    first

---
+++ src/App.svelte
@@ -0,0 +1,469 @@
+<svelte:options tag="fds-image-editor-text"></svelte:options>
+<script>
+    import { onMount } from "svelte"
+    import fdsHelper from "@fds-components/fds-helper"
+   // let path = fdsHelper.get_href()
+    import {get_current_component} from "svelte/internal"
+    let host = get_current_component()
+    import { version as componentversion, name } from "../package.json"
+    // eslint-disable-next-line no-unused-vars
+    import Dialog from './Dialog.svelte';
+        /**
+     * width of canvas
+     * @type {integer}
+     */
+    export let width=500
+    /**
+     * height of canvas
+     * @type {integer}
+     */    
+    export let height=500
+    /**
+     * object with data property to save scene on destroy 
+     */
+    export let external_scene_storage=null
+
+    export function refresh() {
+        layer=layer
+    }
+    /**
+     * Get information about component
+     * @param  {("api" | "examples" | "css")} type the info type
+     */
+    export async function getInfo(type) {
+        if (type === "version") {
+            return new Promise((resolve) => {
+                resolve(componentversion);
+            });
+        }
+        let res = await fdsHelper.getInfo(type, name);
+        return res;
+    }
+
+    /**
+     * version of component
+     * @type {string}
+     */
+    export const version = componentversion;
+
+
+    function _destroy() {
+        if (!external_scene_storage) return
+        external_scene_storage.data=getScene()
+    }
+    onMount(() => {
+        return () => {
+            _destroy()
+        }
+    })    
+    
+
+    export async function prepareForAI(resultLayer) {
+        let imageAPI=globalThis.gyre.imageAPI
+        let canvas=globalThis.gyre.canvas
+        width = resultLayer.width
+        height = resultLayer.height
+        layer.url = await renderForAI()     // set image data
+        width = resultLayer._width
+        height = resultLayer._height
+        let onelayer = [layer] // only one layer
+        let base64 = await imageAPI.getImageDataCombined(onelayer, resultLayer.x, resultLayer.y, resultLayer.width, resultLayer.height, {width:canvas.width,height:canvas.height}, null, true)
+        let scaledBase64 = await imageAPI.scaleImageCanvas(base64, resultLayer.generate.targetWidth, resultLayer.generate.targetHeight)
+        let ScaledImageInfo={}
+        ScaledImageInfo.base64 = ScaledImageInfo.urlRef=await  imageAPI.fillImageBackground(scaledBase64, 'white')
+        ScaledImageInfo.type = 'skribble' // use ControlNet skribble
+        ScaledImageInfo.base64 = ScaledImageInfo.base64.split(',')[1] // no url prefix for base64 needed here
+        resultLayer.specialLayersImageInfo.push(ScaledImageInfo)
+        layer.url=null  // temp render image not needed anymore
+    }
+
+
+    export function addLayer(newlayer) {
+        newlayer.type = 'fds-image-editor-text'
+        newlayer.name = 'Text'
+        newlayer.letter = "T"
+        newlayer.background_type = 'transparent'
+        newlayer.font_size = 200
+        newlayer.string = 'TEXT'
+        newlayer.align="middle"
+        newlayer.bold=false
+        newlayer.italic=false
+        newlayer.underline=false
+        newlayer.letter_spacing = 0
+        newlayer.font = 'Josefin Sans'
+    }
+
+    export async function prepareForSave() {
+        layer.data = getScene()
+        return layer
+    }
+
+    export function getLayerMenuHTML(l,thumbclass,thumbStyle) {
+        // justr render a T as thumb. Could be an image as well
+        let html=
+            '<div ' +
+            thumbclass +
+            ' style="' +
+            thumbStyle +
+            ' font-size:33px;font-weight:bold;display:flex;align-content:space-between;justify-content:space-around;align-items: center;">T</div> ' +
+            l.name
+        return html
+    }
+
+    export async function quickMask() {
+        let imageAPI=globalThis.gyre.imageAPI
+        let canvas=globalThis.gyre.canvas      // this is app main canvas and not an HTML canvas
+        width = layer.width
+        height = layer.height
+        layer.url = await renderForAI()
+        width = layer._width
+        height = layer._height
+        let onelayer = [layer] // only one layer
+        let base64 = await imageAPI.getImageDataCombined(onelayer, 0, 0, canvas.width, canvas.height, {width:canvas.width,height:canvas.height}, null, true)
+        let HTMLCanvas = await imageAPI.base64ToCanvas(base64, canvas.width, canvas.height) // can be HTML canvas or Tiled canvas
+        imageAPI.canvasMakeTransparentWhite(HTMLCanvas)
+        imageAPI.canvasInvert(HTMLCanvas)
+        HTMLCanvas = await imageAPI.canvasExpandMaskFast(HTMLCanvas)
+        let res = await HTMLCanvas.toDataURL()
+        return res
+    }
+    /**
+     * check all styleSheets
+     * external link or imported stylesheets
+     * need to be fetched and inlined
+     * to create font Files object
+     */
+    async function getFontReferences() {
+    let fontFiles = [];
+
+    // inline external css
+    let styleSheets = await inlineExternalCss(true, true);
+
+    styleSheets.forEach((styleSheet, s) => {
+        let cssRules = styleSheet.cssRules;
+        for (let i = 0; i < cssRules.length; i++) {
+        let rule = cssRules[i];
+        let typeRule = rule.type;
+
+        // type===5: is @font-face rule
+        if (typeRule === 5) {
+            let style = rule.style;
+            let fontFamily=style.getPropertyValue("font-family")
+            fontFamily = fontFamily.replaceAll('"', "");
+            let fontWeight=style.getPropertyValue("font-weight")
+            fontWeight = parseFloat(style.fontWeight);
+            let fontStyle=style.getPropertyValue("font-style")
+            fontStyle = style.fontStyle;
+            let unicodeRange = style.unicodeRange;
+
+            let src = style.getPropertyValue("src")
+            .split(",")
+            .map((val) => {
+                return val.split(" ")[0];
+            })
+            .filter(Boolean);
+            let url = "";
+
+            // extract font file URLs/paths
+            src.forEach((fontSrc) => {
+            url = fontSrc.match(/\(([^)]+)\)/)[1].replaceAll('"', "");
+            });
+
+            // add to fontFiles object
+            fontFiles.push({
+            origin: origins[s],
+            fontFamily: fontFamily,
+            fontWeight: fontWeight,
+            fontStyle: fontStyle,
+            src: url,
+            unicodeRange: unicodeRange
+            });
+        }
+        }
+    });
+
+    return fontFiles;
+    }
+
+    /**
+     * inline all external stylesheets
+     */
+
+    // collect all css origins
+    let origins = [];
+
+    async function inlineExternalCss(removeExternalCss = true) {
+    let styleSheets = Array.from(document.styleSheets);
+
+    /**
+     * stylesheet helper
+     * fetch external css
+     */
+    const fetchExternalStylesheet = async (href, ) => {
+        let fetched = await fetch(href);
+        let css = await fetched.text();
+        let newStyle = document.createElement("style");
+        newStyle.classList.add("externalCSS");
+        let externalCss = document.head.querySelectorAll(".externalCSS");
+        if (!externalCss.length) {
+        document.head.insertBefore(newStyle, document.head.children[0]);
+        } else {
+        document.head.insertBefore(
+            newStyle,
+            externalCss[externalCss.length - 1].nextElementSibling
+        );
+        }
+        newStyle.textContent = `/** origin: ${href} **/\n` + css;
+        origins.push(href);
+    };
+
+    for (let i = 0; i < styleSheets.length; i++) {
+        let stylesheet = styleSheets[i];
+        let href = stylesheet.href;
+
+        // fetch external stylesheet
+        if (href) {
+        await fetchExternalStylesheet(href, i);
+        } else {
+        let cssRules = stylesheet.cssRules;
+        let cssRulesL = cssRules.length;
+        for (let r = 0; r < cssRulesL; r++) {
+            let rule = cssRules[r];
+            let type = rule.type;
+
+            // is @import rule - fetch external stylesheet
+            if (type === 3) {
+            let href = rule.href;
+            if (href) {
+                if (removeExternalCss) {
+                stylesheet.deleteRule(r);
+                cssRulesL--;
+                r--;
+                }
+                await fetchExternalStylesheet(href, r);
+            }
+            } else if (type === 5) {
+            origins.push("local");
+            }
+        }
+        }
+    }
+
+    // remove external stylesheet
+    if (removeExternalCss) {
+        let styleSheetLinks = document.querySelectorAll(
+        'link[href$=".css"], link[rel="stylesheet"]'
+        );
+        for (let i = 0; i < styleSheetLinks.length; i++) {
+        let link = styleSheetLinks[i];
+        await link.remove();
+        }
+    }
+
+    let styleSheetsInlined = Array.from(document.styleSheets);
+    return styleSheetsInlined;
+    }
+     async function getFontData() {
+        let fontRefs=await getFontReferences()
+        let fontURL=""
+        for(let f of fontRefs) {
+            if (layer.font===f.fontFamily) {
+                fontURL=f.src
+            }
+        }
+        if (!fontURL)  return 
+        let resp = await fetch(fontURL);
+        let blob = await resp.blob()
+        return new Promise((resolve) => {
+            let f = new FileReader()
+            f.addEventListener('load', () => resolve(f.result))
+            f.readAsDataURL(blob);
+        })
+    }
+    export async function renderForAI() {
+        await _renderForAI()    // twice because of iPad bug
+//        await  new Promise(resolve => setTimeout(resolve, 300));
+        return await _renderForAI()
+    }
+    export async function _renderForAI() {
+                // first load the font
+        let fontData=await getFontData()
+        let style=""
+        if (fontData) {
+            style="<style>\n @font-face { font-family: '"+layer.font+"';"
+            style+="\nsrc: url("+fontData+") format('woff2');\n</style> "
+        }
+           
+        let svgData = new XMLSerializer().serializeToString(svg)
+        svgData=svgData.replace("<defs/>","<defs>"+style+"</defs>")   // embed font
+
+        const svgDataBase64 = btoa(unescape(encodeURIComponent(svgData)))        
+        const svgDataUrl = `data:image/svg+xml;charset=utf-8;base64,${svgDataBase64}`        
+        
+        const canvas = document.createElement('canvas')
+        let image = new Image()
+        return new Promise( (resolve) =>  {
+            image.addEventListener('load',async () => {
+                canvas.width=parseInt( width)
+                canvas.height=parseInt( height)
+                const context = canvas.getContext('2d')
+                context.drawImage(image, 0, 0, width, height)
+                const dataUrl = canvas.toDataURL('image/png')
+                resolve(dataUrl)
+            })
+            image.src = svgDataUrl
+        })
+
+    }
+        
+
+
+
+   
+    export let layer={string:"Hello",font_size:150,align:"middle",background:"transparent",bold:false,italic:false,underline:false} 
+    /* aditional layer data: for every plugin
+        element: point to DOM tree element which contains this component
+        data:    exported serialized data of this layer
+        x,y,width,height: size and position of this layer. both can be changed by move-tool
+        _x,_y,_width,_height: scaled cooedinates
+        url: temporary image data
+        name: the layer name which can be changed by user
+        id: internal ID
+    */ 
+    let fontweight="normal";
+    let fontstyle="normal";
+    let textdecoration="none";
+
+    $: {        
+        if (layer) {
+            if (!layer.align) layer.align="middle"
+            layer=layer
+        }
+        if(layer && layer.bold){
+            fontweight = "bold";
+        }  else{
+            fontweight = "normal";
+        }
+
+        if(layer && layer.italic){
+            fontstyle="italic";
+        }else{
+            fontstyle="normal";
+        }
+
+        if(layer && layer.underline){
+            textdecoration="underline";
+        }else{
+            textdecoration="none";
+        }
+
+
+
+        if (layer && layer.string) {
+            calcText()
+        }
+        if (layer && layer.font_size) {
+            calcText()
+        }
+        if (width || height) {
+            calcInnerSize()
+        }
+    }
+    let innerHeight=1000
+    let innerWidth=1000
+    function calcInnerSize() {
+        let ar=width/height
+        if (width>height) {
+            innerHeight=1000/ar
+            innerWidth=1000
+        } else {
+            innerWidth=1000/ar
+            innerHeight=1000
+        }
+    }
+    let lines=[]
+    let lineHeight
+    let yStart
+    function calcText() {
+        if  (!layer.string) return 
+        lines=layer.string.split("\n")
+        lineHeight=layer.font_size*1.2
+        yStart=-(lineHeight*(lines.length-1))/2
+    }
+
+    export async function autoSize() {
+        let t=host.shadowRoot.querySelector("#text")
+        if (!t) return
+
+        let rect=t.getBoundingClientRect()
+        if (rect.width>=width) {
+            while (rect.width>width) {
+                layer.font_size-=10
+                await new Promise(resolve => setTimeout(resolve, 1))
+                rect=t.getBoundingClientRect()
+            }
+            return
+        }
+        while (rect.width<width) {
+            layer.font_size+=10
+            await new Promise(resolve => setTimeout(resolve, 1))
+            rect=t.getBoundingClientRect()
+        }
+        layer.font_size-=10
+    }
+
+    /**
+     * get full scene data: camera and all poses
+     */
+    export function getScene() {
+        let scene={  text:layer.string, font_size:layer.font_size,font:layer.font,align:layer.align,background:layer.background}
+        let sceneData=JSON.parse(JSON.stringify(scene))
+        return sceneData
+    }
+    /**
+     * set scene and pose data
+     */
+     export async function setScene(sceneInfo) {
+        layer.string=sceneInfo.text
+        layer.font_size=sceneInfo.font_size
+        layer.font=sceneInfo.font
+        layer.align=sceneInfo.align
+        layer.background=sceneInfo.background
+    }
+
+    /**
+     * make image from current scene 
+     * @return: base64-url
+     */
+    export async function renderNormal() {
+//        let url=canvas.toDataURL()             
+//        setMode(oldMode)
+let url=""
+        return url
+    }
+   
+
+
+  
+    
+    /**
+     * canvas can be used somewhere else
+     */
+    export let svg
+</script>
+<svg {width}  {height} style="background:{layer.background}"  viewBox="0 0 {innerWidth} {innerHeight}" bind:this={svg}>
+    <defs></defs>
+
+    <text id="text" x="0" y="{innerHeight/2}"  style="text-decoration:{textdecoration}; font-style:{fontstyle}; font-weight:{fontweight}; font-size:{layer.font_size}px;font-family:'{layer.font}';letter-spacing:{layer.letter_spacing}rem"  dominant-baseline="middle" >
+        {#each lines as text,i}       
+            <tspan x="{innerWidth/2}" y="{innerHeight/2}" dy="{yStart+lineHeight*i}px" text-anchor="{layer.align}">{text}</tspan>
+        {/each}
+    </text>
+  </svg>
+<style>
+
+    * {
+        box-sizing: border-box;
+    }
+    /** delete this CSS: */
+</style>
---
+++ src/Dialog.svelte
@@ -0,0 +1,246 @@
+<svelte:options tag="fds-image-editor-text-toolbar"></svelte:options>
+<script>
+    import {get_current_component} from "svelte/internal"
+    import {clickOutside} from "./clickOutside";
+
+    let fonts = ["Montez", "Lobster", "Josefin Sans", "Shadows Into Light", "Pacifico", "Amatic SC", "Orbitron", "Rokkitt", "Righteous", "Dancing Script", "Bangers", "Chewy", "Sigmar One", "Architects Daughter", "Abril Fatface", "Covered By Your Grace", "Kaushan Script", "Gloria Hallelujah", "Satisfy", "Lobster Two", "Comfortaa", "Cinzel", "Courgette"];
+    let host = get_current_component()
+    let dropdownel;
+    export let values = {
+        selectedLayer: {
+            font_size: 150,
+            letter_spacing: 1,
+            bold: false,
+            italic: false,
+            underline: false
+        }
+    }
+
+    function action(name, value) {
+        host.dispatchEvent(new CustomEvent("action", {detail: {name: name, value: value}}))
+    }
+
+    function change(name, value) {
+        host.dispatchEvent(new CustomEvent("change", {detail: {name: name, value: value}}))
+    }
+
+    function handleClickOutside() {
+        dropdownel.classList.remove("show");
+    }
+
+    function toggleDropdownVisibility() {
+        dropdownel.classList.toggle("show");
+    }
+
+    $: if (values) {
+        values = values
+    }
+
+    let editText
+
+</script>
+
+<div class="show">
+    {#if values.selectedLayer}
+        <div style="display:flex;align-items: center">
+            Text |
+            Font Size: <span class="values3" style="display: inline-block; margin-right: 10px">{values.selectedLayer.font_size}</span>
+            <fds-slider
+                    class="slider"
+                    min="50"
+                    max="900"
+                    on:input={(e) => { values.selectedLayer.font_size=e.target.value;values=values;change("change","font_size") }}
+                    value={values.selectedLayer.font_size}
+                    style="margin-right:20px"
+            ></fds-slider>
+
+            <!-- svelte-ignore a11y-click-events-have-key-events -->
+            <fds-image-editor-button type="button" on:click={() => { editText=!editText }}>Text/Font...</fds-image-editor-button>
+        </div>
+    {/if}
+
+    {#if editText && values.selectedLayer }
+        <div class="editText">
+
+            <div class="dropdown" use:clickOutside on:click_outside={handleClickOutside}>
+                <button
+                        class="font-preview"
+                        style="font-family:{values.selectedLayer.font}"
+                        on:click={() => {toggleDropdownVisibility() }}
+                >{values.selectedLayer.font}</button>
+                <div id="myDropdown" bind:this={dropdownel} class="dropdown-content">
+                    {#each fonts as font}
+                        <a on:click={(e) => {  values.selectedLayer.font=e.target.text;values=values;change("change","font") }} href="#"
+                           style="font-family:{font}">{font}</a>
+                    {/each}
+                </div>
+            </div>
+
+            <div style="margin-top: 16px">
+                Letter Spacing: <span class="values3">{values.selectedLayer.letter_spacing}</span>
+            </div>
+            <div style="margin-top: 6px">
+                <fds-slider
+                        min="-2"
+                        max="8"
+                        step="0.1"
+                        on:input={(e) => { values.selectedLayer.letter_spacing=e.target.value;values=values;change("change","letter_spacing") }}
+                        value={values.selectedLayer.letter_spacing}
+                        style=""
+                ></fds-slider>
+            </div>
+            <div class="buttoncontainer">
+                <!-- svelte-ignore a11y-click-events-have-key-events -->
+                <fds-image-editor-button
+                        icon="bold"
+                        title="Bold"
+                        on:click={() => { values.selectedLayer.bold=!values.selectedLayer.bold;change("change","font_weight") }}
+                        state={values.selectedLayer.bold ? 'active' : ''}
+                ></fds-image-editor-button>
+                <!-- svelte-ignore a11y-click-events-have-key-events -->
+                <fds-image-editor-button
+                        icon="italic"
+                        title="Italic"
+                        on:click={() => { values.selectedLayer.italic=!values.selectedLayer.italic;change("change","font_style"); }}
+                        state={values.selectedLayer.italic ? 'active' : ''}
+                ></fds-image-editor-button>
+                <!-- svelte-ignore a11y-click-events-have-key-events -->
+                <fds-image-editor-button
+                        icon="underline"
+                        title="Underline"
+                        on:click={() => {values.selectedLayer.underline=!values.selectedLayer.underline;change("change","text_decoration"); }}
+                        state={values.selectedLayer.underline ? 'active' : ''}
+                ></fds-image-editor-button>
+            </div>
+
+            <div class="formLine">
+                <textarea class="formInput" style="height:100px;font-size:20px;font-family:{values.selectedLayer.font}"
+                          on:change={(e) => { values.selectedLayer.string=e.target.value;values=values;change("change","text") }}>{values.selectedLayer.string}</textarea>
+            </div>
+        </div>
+    {/if}
+</div>
+
+<style>
+    * {
+        box-sizing: border-box;
+    }
+
+    .buttoncontainer {
+        margin-top: 10px;
+        margin-bottom: 10px;
+        padding-bottom: 5px;;
+        border-bottom: 1px solid white;
+    }
+
+    button {
+        color: white;
+        background-color: transparent;
+    }
+
+    .editText {
+        min-width: 200px;
+        min-height: 100px;
+        box-shadow: 0 0 1rem 0 rgba(255, 255, 255, .2);
+        border-radius: 8px;
+        background-color: rgba(0, 0, 0, .65);
+        backdrop-filter: blur(10px);
+        color: rgba(255, 255, 255, 0.6);
+        position: absolute;
+        left: 150px;
+        top: 50px;
+        padding: 14px;
+        font-family: "Segoe UI", Roboto, system-ui;
+        z-index: 2000;
+        margin-left: 100px;
+    }
+
+    .formLine {
+        displaY: block;
+        margin-bottom: 20px;
+    }
+
+    .slider {
+        vertical-align: middle;
+    }
+
+    .values3 {
+        width: 30px;
+        text-align: right;
+        display: inline-block;
+    }
+
+    .formInput {
+        width: 100%;
+        outline: 0;
+        border: 0;
+        border-bottom: 2px solid rgba(255, 255, 255, 0.2);
+        padding: 5px;
+        font-size: 14px;
+        font-weight: normal;
+        border-radius: 3px;
+        color: rgba(255, 255, 255, 0.9);
+        margin-bottom: 10px;
+        display: block;
+        background: none;
+        font-family: system-ui, "Segoe UI", Roboto;
+
+    }
+
+    .formInput::placeholder {
+        font-weight: normal;
+        opacity: 0.5;
+        color: rgba(255, 255, 255, 0.7);
+    }
+
+    .formLine {
+        margin-bottom: 5px;
+    }
+
+    .dropdown {
+        position: relative;
+        display: inline-block;
+    }
+
+    .dropdown-content {
+        display: none;
+        position: absolute;
+        background-color: #f1f1f1;
+        min-width: 160px;
+        overflow: auto;
+        box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
+        z-index: 1;
+        max-height: calc(100vh / 2);
+        margin-top: 5px;
+        border-radius: 6px;
+    }
+
+    .dropdown-content a {
+        color: black;
+        padding: 12px 16px;
+        text-decoration: none;
+        display: block;
+        font-size: 20px;
+    }
+
+    .dropdown a:hover {
+        background-color: #ddd;
+    }
+
+    .show {
+        display: block;
+    }
+
+    .font-preview {
+        display:inline-block;
+        width:200px;
+        color:black;
+        background-color:white;
+        font-size:20px;
+        padding: 16px;
+        border: none;
+        cursor: pointer;
+        border-radius: 6px;
+    }
+
+</style>
