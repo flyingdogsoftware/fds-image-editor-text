fa9e70a7325a544575c0d6d141d331170656fc5d
Nicolay Mausz <mausz@flyingdog.de>
2024-07-28 22:48:01 +0200

    changes layer export

--- src/Dialog.svelte
+++ src/Dialog.svelte
@@ -6,14 +6,12 @@
     let fonts = ["Montez", "Lobster", "Josefin Sans", "Shadows Into Light", "Pacifico", "Amatic SC", "Orbitron", "Rokkitt", "Righteous", "Dancing Script", "Bangers", "Chewy", "Sigmar One", "Architects Daughter", "Abril Fatface", "Covered By Your Grace", "Kaushan Script", "Gloria Hallelujah", "Satisfy", "Lobster Two", "Comfortaa", "Cinzel", "Courgette"];
     let host = get_current_component()
     let dropdownel;
-    export let values = {
-        selectedLayer: {
+    export let layer = {
             font_size: 150,
             letter_spacing: 1,
             bold: false,
             italic: false,
             underline: false
-        }
     }
 
     function action(name, value) {
@@ -32,27 +30,27 @@
         dropdownel.classList.toggle("show");
     }
 
-    $: if (values) {
-        values = values
+    $: if (layer) {
+        layer = layer
     }
 
     let editText
     export function refresh() {
-        values = values
+        layer = layer
     }
 </script>
 
 <div class="show">
-    {#if values.selectedLayer}
+    {#if layer}
         <div style="display:flex;align-items: center">
             Text |
-            Font Size: <span class="values3" style="display: inline-block; margin-right: 10px">{values.selectedLayer.font_size}</span>
+            Font Size: <span class="values3" style="display: inline-block; margin-right: 10px">{layer.font_size}</span>
             <fds-image-editor-slider
                     class="slider"
                     min="50"
                     max="900"
-                    on:input={(e) => { values.selectedLayer.font_size=e.target.value;values=values;change("change","font_size") }}
-                    value={values.selectedLayer.font_size}
+                    on:input={(e) => { layer.font_size=e.target.value;layer=layer;change("change","font_size") }}
+                    value={layer.font_size}
                     style="margin-right:20px"
             ></fds-image-editor-slider>
 
@@ -61,33 +59,33 @@
         </div>
     {/if}
 
-    {#if editText && values.selectedLayer }
+    {#if editText && layer }
         <div class="editText">
 
             <div class="dropdown" use:clickOutside on:click_outside={handleClickOutside}>
                 <button
                         class="font-preview"
-                        style="font-family:{values.selectedLayer.font}"
+                        style="font-family:{layer.font}"
                         on:click={() => {toggleDropdownVisibility() }}
-                >{values.selectedLayer.font}</button>
+                >{layer.font}</button>
                 <div id="myDropdown" bind:this={dropdownel} class="dropdown-content">
                     {#each fonts as font}
-                        <a on:click={(e) => {  values.selectedLayer.font=e.target.text;values=values;change("change","font") }} href="#"
+                        <a on:click={(e) => {  layer.font=e.target.text;layer=layer;change("change","font") }} href="#"
                            style="font-family:{font}">{font}</a>
                     {/each}
                 </div>
             </div>
 
             <div style="margin-top: 16px">
-                Letter Spacing: <span class="values3">{values.selectedLayer.letter_spacing}</span>
+                Letter Spacing: <span class="values3">{layer.letter_spacing}</span>
             </div>
             <div style="margin-top: 6px">
                 <fds-image-editor-slider
                         min="-2"
                         max="8"
                         step="0.1"
-                        on:input={(e) => { values.selectedLayer.letter_spacing=e.target.value;values=values;change("change","letter_spacing") }}
-                        value={values.selectedLayer.letter_spacing}
+                        on:input={(e) => { layer.letter_spacing=e.target.value;layer=layer;change("change","letter_spacing") }}
+                        value={layer.letter_spacing}
                         style=""
                 ></fds-image-editor-slider>
             </div>
@@ -96,28 +94,28 @@
                 <fds-image-editor-button
                         icon="bold"
                         title="Bold"
-                        on:click={() => { values.selectedLayer.bold=!values.selectedLayer.bold;change("change","font_weight") }}
-                        state={values.selectedLayer.bold ? 'active' : ''}
+                        on:click={() => { layer.bold=!layer.bold;change("change","font_weight") }}
+                        state={layer.bold ? 'active' : ''}
                 ></fds-image-editor-button>
                 <!-- svelte-ignore a11y-click-events-have-key-events -->
                 <fds-image-editor-button
                         icon="italic"
                         title="Italic"
-                        on:click={() => { values.selectedLayer.italic=!values.selectedLayer.italic;change("change","font_style"); }}
-                        state={values.selectedLayer.italic ? 'active' : ''}
+                        on:click={() => { layer.italic=!layer.italic;change("change","font_style"); }}
+                        state={layer.italic ? 'active' : ''}
                 ></fds-image-editor-button>
                 <!-- svelte-ignore a11y-click-events-have-key-events -->
                 <fds-image-editor-button
                         icon="underline"
                         title="Underline"
-                        on:click={() => {values.selectedLayer.underline=!values.selectedLayer.underline;change("change","text_decoration"); }}
-                        state={values.selectedLayer.underline ? 'active' : ''}
+                        on:click={() => {layer.underline=!layer.underline;change("change","text_decoration"); }}
+                        state={layer.underline ? 'active' : ''}
                 ></fds-image-editor-button>
             </div>
 
             <div class="formLine">
-                <textarea class="formInput" style="height:100px;font-size:20px;font-family:{values.selectedLayer.font}"
-                          on:change={(e) => { values.selectedLayer.string=e.target.value;values=values;change("change","text") }}>{values.selectedLayer.string}</textarea>
+                <textarea class="formInput" style="height:100px;font-size:20px;font-family:{layer.font}"
+                          on:change={(e) => { layer.string=e.target.value;layer=layer;change("change","text") }}>{layer.string}</textarea>
             </div>
         </div>
     {/if}
